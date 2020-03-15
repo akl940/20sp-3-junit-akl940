59d887e919afbea7e4d2a70a64c19a29c0d4e0ac
akl940 <59671045+akl940@users.noreply.github.com>
2020-03-14 20:33:31 -0400

    new tests

--- TestMySlots.java
+++ TestMySlots.java
@@ -1,12 +1,12 @@
 import static org.junit.jupiter.api.Assertions.*;
 
-import static org.junit.jupiter.api.Assertions.*;
-
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestMySlots {
 
 	@Test
+	@DisplayName("The array has a length of 5")
 	void fiveElementsInArrayTest() {
 		BetterSlot s = new BetterSlot();
 
@@ -15,22 +15,22 @@ class TestMySlots {
 	}
 	
 	/*@Test elementsInTheValidRangeTest() {
-		SlipperySlot s = new SlipperySlot();
+		BetterSlot s = new BetterSlot();
 		
 		int array[] = s.pullTheLever();
 	}*/
 	
 	@Test
+	@DisplayName("All 5 match")
 	void fiveMatchTest() {
 		BetterSlot s = new BetterSlot();
 		int array[] = new int[] {5, 5, 5, 5, 5};
 		
-		assertEquals(1000000, s.payOff(array));
-		
-		//fail("Not yet implemented");
+		assertEquals(1000000, s.payOff(array));		
 	}
 
 	@Test
+	@DisplayName("4 match")
 	void fourMatchTest() {
 		BetterSlot s = new BetterSlot();
 		int array[] = new int[] {5, 7, 5, 5, 5};
@@ -39,6 +39,7 @@ class TestMySlots {
 	}
 	
 	@Test
+	@DisplayName("3 and remaining 2 match")
 	void threeAndTwoMatchTest() {
 		BetterSlot s = new BetterSlot();
 		int array[] = new int[] {5, 3, 5, 3, 5};
@@ -47,6 +48,7 @@ class TestMySlots {
 	}
 	
 	@Test
+	@DisplayName("Only 3 match")
 	void onlyThreeMatchTest() {
 		BetterSlot s = new BetterSlot();
 		int array[] = new int[] {5, 3, 5, 5, 10};
@@ -55,6 +57,7 @@ class TestMySlots {
 	}
 	
 	@Test
+	@DisplayName("Only 2 match")
 	void onlyTwoMatchTest() {
 		BetterSlot s = new BetterSlot();
 		int array[] = new int[] {5, 7, 3, 5, 10};
@@ -63,6 +66,7 @@ class TestMySlots {
 	}
 	
 	@Test
+	@DisplayName("42")
 	void One42Test() {
 		BetterSlot s = new BetterSlot();
 		int array[] = new int[] {5, 7, 3, 42, 10};
@@ -71,6 +75,7 @@ class TestMySlots {
 	}
 	
 	@Test 
+	@DisplayName("42; two others match")
 	void One42AndTwoMatching() {
 		BetterSlot s = new BetterSlot();
 		int array[] = new int[] {5, 7, 5, 42, 10};
@@ -79,6 +84,7 @@ class TestMySlots {
 	}
 	
 	@Test
+	@DisplayName("Two (matching) 42s")
 	void two42Test() {
 		BetterSlot s = new BetterSlot();
 		int array[] = new int[] {5, 42, 3, 42, 10};
@@ -87,11 +93,83 @@ class TestMySlots {
 	}
 	
 	@Test
+	@DisplayName("Three (matching) 42s")
 	void three42Test() {
 		BetterSlot s = new BetterSlot();
 		int array[] = new int[] {5, 42, 3, 42, 42};
 	
 		assertEquals(102, s.payOff(array));
 	}
-
+	
+	@Test
+	@DisplayName("A perfect square")
+	void perfSq() {
+		BetterSlot s = new BetterSlot();
+		int array[] = new int[] {5, 7, 3, 9, 11};
+	
+		assertEquals(7, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("Two different perfect squares")
+	void twoPerfSq() {
+		BetterSlot s = new BetterSlot();
+		int array[] = new int[] {5, 7, 25, 9, 11};
+	
+		assertEquals(7, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("Two matching perfect squares")
+	void matchPerfSq() {
+		BetterSlot s = new BetterSlot();
+		int array[] = new int[] {5, 7, 9, 9, 11};
+	
+		assertEquals(17, s.payOff(array));
+	}
+	
+	@Test 
+	@DisplayName("A perfect square; two others match")
+	void perfSqAndTwoMatching() {
+		BetterSlot s = new BetterSlot();
+		int array[] = new int[] {5, 7, 5, 9, 10};
+	
+		assertEquals(17, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("A power of 2")
+	void pow2() {
+		BetterSlot s = new BetterSlot();
+		int array[] = new int[] {5, 7, 3, 8, 11};
+	
+		assertEquals(3, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("Two different powers of 2")
+	void twoPows2() {
+		BetterSlot s = new BetterSlot();
+		int array[] = new int[] {5, 7, 32, 8, 11};
+	
+		assertEquals(3, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("Two matching powers of 2")
+	void matchPows2() {
+		BetterSlot s = new BetterSlot();
+		int array[] = new int[] {5, 7, 8, 8, 11};
+	
+		assertEquals(13, s.payOff(array));
+	}
+	
+	@Test 
+	@DisplayName("A power of 2; two others match")
+	void pow2AndTwoMatching() {
+		BetterSlot s = new BetterSlot();
+		int array[] = new int[] {5, 8, 5, 41, 10};
+	
+		assertEquals(13, s.payOff(array));
+	}
 }
--- TestSlipperySlot.java
+++ TestSlipperySlot.java
@@ -1,10 +1,12 @@
 import static org.junit.jupiter.api.Assertions.*;
 
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 class TestSlipperySlot {
 
 	@Test
+	@DisplayName("The array has a length of 5")
 	void fiveElementsInArrayTest() {
 		SlipperySlot s = new SlipperySlot();
 
@@ -19,16 +21,16 @@ class TestSlipperySlot {
 	}*/
 	
 	@Test
+	@DisplayName("All 5 match")
 	void fiveMatchTest() {
 		SlipperySlot s = new SlipperySlot();
 		int array[] = new int[] {5, 5, 5, 5, 5};
 		
-		assertEquals(1000000, s.payOff(array));
-		
-		//fail("Not yet implemented");
+		assertEquals(1000000, s.payOff(array));		
 	}
 
 	@Test
+	@DisplayName("4 match")
 	void fourMatchTest() {
 		SlipperySlot s = new SlipperySlot();
 		int array[] = new int[] {5, 7, 5, 5, 5};
@@ -37,6 +39,7 @@ class TestSlipperySlot {
 	}
 	
 	@Test
+	@DisplayName("3 and remaining 2 match")
 	void threeAndTwoMatchTest() {
 		SlipperySlot s = new SlipperySlot();
 		int array[] = new int[] {5, 3, 5, 3, 5};
@@ -45,6 +48,7 @@ class TestSlipperySlot {
 	}
 	
 	@Test
+	@DisplayName("Only 3 match")
 	void onlyThreeMatchTest() {
 		SlipperySlot s = new SlipperySlot();
 		int array[] = new int[] {5, 3, 5, 5, 10};
@@ -53,6 +57,7 @@ class TestSlipperySlot {
 	}
 	
 	@Test
+	@DisplayName("Only 2 match")
 	void onlyTwoMatchTest() {
 		SlipperySlot s = new SlipperySlot();
 		int array[] = new int[] {5, 7, 3, 5, 10};
@@ -61,6 +66,7 @@ class TestSlipperySlot {
 	}
 	
 	@Test
+	@DisplayName("42")
 	void One42Test() {
 		SlipperySlot s = new SlipperySlot();
 		int array[] = new int[] {5, 7, 3, 42, 10};
@@ -69,6 +75,7 @@ class TestSlipperySlot {
 	}
 	
 	@Test 
+	@DisplayName("42; two others match")
 	void One42AndTwoMatching() {
 		SlipperySlot s = new SlipperySlot();
 		int array[] = new int[] {5, 7, 5, 42, 10};
@@ -77,6 +84,7 @@ class TestSlipperySlot {
 	}
 	
 	@Test
+	@DisplayName("Two (matching) 42s")
 	void two42Test() {
 		SlipperySlot s = new SlipperySlot();
 		int array[] = new int[] {5, 42, 3, 42, 10};
@@ -85,10 +93,83 @@ class TestSlipperySlot {
 	}
 	
 	@Test
+	@DisplayName("Three (matching) 42s")
 	void three42Test() {
 		SlipperySlot s = new SlipperySlot();
 		int array[] = new int[] {5, 42, 3, 42, 42};
 	
 		assertEquals(102, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("A perfect square")
+	void perfSq() {
+		SlipperySlot s = new SlipperySlot();
+		int array[] = new int[] {5, 7, 3, 9, 11};
+	
+		assertEquals(7, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("Two different perfect squares")
+	void twoPerfSq() {
+		SlipperySlot s = new SlipperySlot();
+		int array[] = new int[] {5, 7, 25, 9, 11};
+	
+		assertEquals(7, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("Two matching perfect squares")
+	void matchPerfSq() {
+		SlipperySlot s = new SlipperySlot();
+		int array[] = new int[] {5, 7, 9, 9, 11};
+	
+		assertEquals(17, s.payOff(array));
+	}
+	
+	@Test 
+	@DisplayName("A perfect square; two others match")
+	void perfSqAndTwoMatching() {
+		SlipperySlot s = new SlipperySlot();
+		int array[] = new int[] {5, 7, 5, 9, 10};
+	
+		assertEquals(17, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("A power of 2")
+	void pow2() {
+		SlipperySlot s = new SlipperySlot();
+		int array[] = new int[] {5, 7, 3, 8, 11};
+	
+		assertEquals(3, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("Two different powers of 2")
+	void twoPows2() {
+		SlipperySlot s = new SlipperySlot();
+		int array[] = new int[] {5, 7, 32, 8, 11};
+	
+		assertEquals(3, s.payOff(array));
+	}
+	
+	@Test
+	@DisplayName("Two matching powers of 2")
+	void matchPows2() {
+		SlipperySlot s = new SlipperySlot();
+		int array[] = new int[] {5, 7, 8, 8, 11};
+	
+		assertEquals(13, s.payOff(array));
+	}
+	
+	@Test 
+	@DisplayName("A power of 2; two others match")
+	void pow2AndTwoMatching() {
+		SlipperySlot s = new SlipperySlot();
+		int array[] = new int[] {5, 8, 5, 42, 10};
+	
+		assertEquals(13, s.payOff(array));
 	}
 }
